1aa49e250feaf05de3569f116660cbff74d2b5ed
julienDaoustCistec <jdaoust@cistec.ca>
2017-02-20 16:37:39 -0500

    getData()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
 
+import { HttpService } from "./http.service";
+
+
 @Component({
   selector: 'http-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'http works!';
+  items: any[] = [];
+  
+  constructor(private httpService: HttpService) { }
+
+  onSubmit(username: string, email: string) {
+    this.httpService.sendData({ username: username, email: email })
+      .subscribe(
+      data => console.log(data)
+      );
+  }
+
+  onGetData() {
+    this.httpService.getOwnData()
+    .subscribe(
+      data => {
+        const myArray = [];
+        for (let key in data) {
+            myArray.push(data[key]);
+        }
+        this.items = myArray;
+      }
+    );
+  }
 }
