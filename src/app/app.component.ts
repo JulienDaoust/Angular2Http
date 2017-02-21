4f31ea7d80003548cd76701d90499493a152e80a
julienDaoustCistec <jdaoust@cistec.ca>
2017-02-21 09:09:16 -0500

    http handle error and done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,13 +9,15 @@ import { HttpService } from "./http.service";
 })
 export class AppComponent {
   items: any[] = [];
+  asyncString = this.httpService.getData();
   
   constructor(private httpService: HttpService) { }
 
   onSubmit(username: string, email: string) {
     this.httpService.sendData({ username: username, email: email })
       .subscribe(
-      data => console.log(data)
+      data => console.log(data),
+      error => console.log(error)
       );
   }
 
